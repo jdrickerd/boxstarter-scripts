77604aec34e159821603fc10c2ec61da0d6ae939
Josiah Rickerd <jdrickerd@gmail.com>
2021-05-03 10:25:37 -0400

    Update dev_std.ps1

--- dev_std.ps1
+++ dev_std.ps1
@@ -4,34 +4,16 @@
 
 Disable-UAC
 
-#--- Windows Features ---
-Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
-
-#--- File Explorer Settings ---
-Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1
-Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
-Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
-Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
 
 #--- Windows Subsystems/Features ---
 choco install -y Microsoft-Hyper-V-All -source windowsFeatures
 choco install -y Microsoft-Windows-Subsystem-Linux -source windowsfeatures
 
-#--- Ubuntu ---
-Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1604 -OutFile ~/Ubuntu.appx -UseBasicParsing
-Add-AppxPackage -Path ~/Ubuntu.appx
-
-#--- Fonts ---
-choco install -y inconsolata
-choco install -y hackfont
-
 #--- Languages ---
-choco install -y python
-choco install -y pip
+choco install -y python3
 choco install nodejs
 
 #--- Tools ---
-choco install -y docker-for-windows
 choco install -y 7zip.install
 choco install -y notepadplusplus
 choco install -y git
@@ -39,9 +21,6 @@ choco install -y putty
 choco install -y winscp
 choco install -y awscli
 choco install -y tortoisesvn
-
-#--- Browsers ---
-choco install -y googlechrome
 
 #--- VS Code ---
 choco install -y visualstudiocode
